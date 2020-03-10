1f9e5b85bf29fbc17d5ac712beaf7914a8b031fc
sofiacarena <57421202+sofiacarena@users.noreply.github.com>
2020-03-10 20:45:24 -0300

    Delete misguifos.js

--- misguifos.js
+++
@@ -1,5 +0,0 @@
-// valor de 'gifs_id' en local storage
-let gifsIdsStorage = localStorage.getItem('gifs_id');
-// ejecución de getSaveGifByIDAndGrid que va a generar el grid 
-//donde se van a ver los gifs creados utilizando los ids guardados en localStorage
-if (gifsIdsStorage) {getSaveGifByIDAndGrid('misguifos_container', 'misguifos', gifsIdsStorage)};
